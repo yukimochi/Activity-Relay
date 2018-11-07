754fca8cb8374de1ebe0a874e8a8fa72c7457551
Naoki Kosaka <n.k@mail.yukimochi.net>
2018-11-07 20:42:04 +0900

    Bump version 0.0.2.

--- ActivityPub/activity.go
+++ ActivityPub/activity.go
@@ -19,7 +19,7 @@ import (
 	"github.com/yukimochi/httpsig"
 )
 
-var UA_STRING = "YUKIMOCHI Toot Relay Service (golang net/http; Activity-Relay v0.0.1; " + os.Getenv("RELAY_DOMAIN") + ")"
+var UA_STRING = "YUKIMOCHI Toot Relay Service (golang net/http; Activity-Relay v0.0.2; " + os.Getenv("RELAY_DOMAIN") + ")"
 
 func appendSignature(r *http.Request, body *[]byte, KeyID string, pKey *rsa.PrivateKey) error {
 	hash := sha256.New()
