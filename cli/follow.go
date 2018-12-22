93a00a13e2032d962a0c7ac0d332cabc5e5659c2
Naoki Kosaka <n.k@mail.yukimochi.net>
2018-12-22 23:05:57 +0900

    Use cobra in CLI. (#10)

--- cli/follow.go
+++ cli/follow.go
@@ -7,9 +7,46 @@ import (
 	"strings"
 
 	"github.com/RichardKnop/machinery/v1/tasks"
-	"github.com/urfave/cli"
+	"github.com/spf13/cobra"
 	"github.com/yukimochi/Activity-Relay/ActivityPub"
+	"github.com/yukimochi/Activity-Relay/State"
 )
+
+func followCmdInit() *cobra.Command {
+	var follow = &cobra.Command{
+		Use:   "follow",
+		Short: "Manage follow request for relay",
+		Long:  "List all follow request and accept/reject follow requests.",
+	}
+
+	var followList = &cobra.Command{
+		Use:   "list",
+		Short: "List follow request",
+		Long:  "List follow request.",
+		RunE:  listFollowsC,
+	}
+	follow.AddCommand(followList)
+
+	var followAccept = &cobra.Command{
+		Use:   "accept",
+		Short: "Accept follow request",
+		Long:  "Accept follow request by domain.",
+		Args:  cobra.MinimumNArgs(1),
+		RunE:  acceptFollowC,
+	}
+	follow.AddCommand(followAccept)
+
+	var followReject = &cobra.Command{
+		Use:   "reject",
+		Short: "Reject follow request",
+		Long:  "Reject follow request by domain.",
+		Args:  cobra.MinimumNArgs(1),
+		RunE:  rejectFollowC,
+	}
+	follow.AddCommand(followReject)
+
+	return follow
+}
 
 func pushRegistorJob(inboxURL string, body []byte) {
 	job := &tasks.Signature{
@@ -34,12 +71,10 @@ func pushRegistorJob(inboxURL string, body []byte) {
 	}
 }
 
-func listFollows(c *cli.Context) error {
-	var err error
+func listFollowsC(cmd *cobra.Command, args []string) error {
 	var domains []string
-
-	fmt.Println(" - Follow request :")
-	follows, err := redClient.Keys("relay:pending:*").Result()
+	cmd.Println(" - Follow request :")
+	follows, err := relayState.RedisClient.Keys("relay:pending:*").Result()
 	if err != nil {
 		return err
 	}
@@ -47,80 +82,88 @@ func listFollows(c *cli.Context) error {
 		domains = append(domains, strings.Replace(follow, "relay:pending:", "", 1))
 	}
 	for _, domain := range domains {
-		fmt.Println(domain)
+		cmd.Println(domain)
 	}
+	cmd.Println(fmt.Sprintf("Total : %d", len(domains)))
+
 	return nil
 }
 
-func acceptFollow(c *cli.Context) error {
-	domain := c.String("domain")
-	if domain != "" {
-		num, err := redClient.Exists("relay:pending:" + domain).Result()
-		if err != nil {
-			return err
-		}
-		if num == 0 {
-			fmt.Println("Given domain not found.")
-			return nil
-		}
-
-		fmt.Println("Accept Follow request : " + domain)
-		data, err := redClient.HGetAll("relay:pending:" + domain).Result()
-		if err != nil {
-			return err
-		}
-		activity := activitypub.Activity{
-			Context: []string{"https://www.w3.org/ns/activitystreams", "https://w3id.org/security/v1"},
-			ID:      data["activity_id"],
-			Actor:   data["actor"],
-			Type:    data["type"],
-			Object:  data["object"],
-		}
-
-		resp := activity.GenerateResponse(hostname, "Accept")
-		jsonData, _ := json.Marshal(&resp)
-
-		pushRegistorJob(data["inbox_url"], jsonData)
-		redClient.HSet("relay:subscription:"+domain, "inbox_url", data["inbox_url"])
-		redClient.Del("relay:pending:" + domain)
-	} else {
-		fmt.Println("No domain given.")
+func createFollowRequestResponse(domain string, response string) error {
+	data, err := relayState.RedisClient.HGetAll("relay:pending:" + domain).Result()
+	if err != nil {
+		return err
 	}
+	activity := activitypub.Activity{
+		Context: []string{"https://www.w3.org/ns/activitystreams", "https://w3id.org/security/v1"},
+		ID:      data["activity_id"],
+		Actor:   data["actor"],
+		Type:    data["type"],
+		Object:  data["object"],
+	}
+
+	resp := activity.GenerateResponse(hostname, response)
+	jsonData, _ := json.Marshal(&resp)
+	pushRegistorJob(data["inbox_url"], jsonData)
+	relayState.RedisClient.Del("relay:pending:" + domain)
+	if response == "Accept" {
+		relayState.AddSubscription(state.Subscription{
+			Domain:     domain,
+			InboxURL:   data["inbox_url"],
+			ActivityID: data["activity_id"],
+			ActorID:    data["actor"],
+		})
+	}
+
 	return nil
 }
 
-func rejectFollow(c *cli.Context) error {
-	domain := c.String("domain")
-	if domain != "" {
-		num, err := redClient.Exists("relay:pending:" + domain).Result()
-		if err != nil {
-			return err
-		}
-		if num == 0 {
-			fmt.Println("Given domain not found.")
-			return nil
-		}
-
-		fmt.Println("Reject Follow request : " + domain)
-		data, err := redClient.HGetAll("relay:pending:" + domain).Result()
-		if err != nil {
-			return err
-		}
-		activity := activitypub.Activity{
-			Context: []string{"https://www.w3.org/ns/activitystreams", "https://w3id.org/security/v1"},
-			ID:      data["activity_id"],
-			Actor:   data["actor"],
-			Type:    data["type"],
-			Object:  data["object"],
-		}
-
-		resp := activity.GenerateResponse(hostname, "Reject")
-		jsonData, _ := json.Marshal(&resp)
-
-		pushRegistorJob(data["inbox_url"], jsonData)
-		redClient.Del("relay:pending:" + domain)
-	} else {
-		fmt.Println("No domain given.")
+func acceptFollowC(cmd *cobra.Command, args []string) error {
+	var err error
+	var domains []string
+	follows, err := relayState.RedisClient.Keys("relay:pending:*").Result()
+	if err != nil {
+		return err
 	}
+	for _, follow := range follows {
+		domains = append(domains, strings.Replace(follow, "relay:pending:", "", 1))
+	}
+
+	for _, domain := range args {
+		for _, request := range domains {
+			if domain == request {
+				cmd.Println("Accept [" + domain + "] follow request")
+				createFollowRequestResponse(domain, "Accept")
+				break
+			}
+		}
+		cmd.Println("Invalid domain given")
+	}
+
+	return nil
+}
+
+func rejectFollowC(cmd *cobra.Command, args []string) error {
+	var err error
+	var domains []string
+	follows, err := redClient.Keys("relay:pending:*").Result()
+	if err != nil {
+		return err
+	}
+	for _, follow := range follows {
+		domains = append(domains, strings.Replace(follow, "relay:pending:", "", 1))
+	}
+
+	for _, domain := range args {
+		for _, request := range domains {
+			if domain == request {
+				cmd.Println("Reject [" + domain + "] follow request")
+				createFollowRequestResponse(domain, "Reject")
+				break
+			}
+		}
+		cmd.Println("Invalid domain given")
+	}
+
 	return nil
 }
